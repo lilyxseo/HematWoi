c388a46650fa73ccd094d74c81046fdbf655dfb0
Abi Baydarus <lilyxseo@gmail.com>
2025-09-14 23:41:28 +0700

    cape jing

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,11 @@
-// tailwind.config.cjs
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html","./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      colors: { brand: "#3898f8" },
+      colors: {
+        brand: "#3898f8",
+      },
     },
   },
   plugins: [],
