ab988aa45c1dc204c94a3c7a67554cffd338cc36
LilyXSeo <149904626+lilyxseo@users.noreply.github.com>
2025-09-25 20:37:15 +0700

    Add React Query provider to main entry

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,17 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
 import "./index.css";
 import { initSyncEngine } from "./lib/sync/SyncEngine";
 
+const queryClient = new QueryClient();
+
 initSyncEngine();
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </BrowserRouter>
 );
