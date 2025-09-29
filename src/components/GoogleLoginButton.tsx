c61308edd8d7145c457d88ffdc0761a5c23c7e34
LilyXSeo <149904626+lilyxseo@users.noreply.github.com>
2025-09-29 22:33:27 +0700

    Update GoogleLoginButton.tsx

--- src/components/GoogleLoginButton.tsx
+++ src/components/GoogleLoginButton.tsx
@@ -62,12 +62,12 @@ const envNativeTrigger =
 const DEFAULT_GOOGLE_WEB_LOGIN_URL = resolveUrl(
   envWebLogin,
   '/auth/google',
-  'https://hw.bydev.me/auth/google'
+  'https://www.hemat-woi.me/auth/google'
 );
 const DEFAULT_NATIVE_TRIGGER_URL = resolveUrl(
   envNativeTrigger,
   '/auth/mobile/google',
-  'https://hw.bydev.me/auth/mobile/google',
+  'https://www.hemat-woi.me/auth/mobile/google',
   { allowCustomScheme: true }
 );
 
